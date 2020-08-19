d143342dc8222b62e464c7574ef4a00bffb637fa
teramotodaiki <i101323@gmail.com>
2020-08-19 18:20:08 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Scratch Fullscreen
+
+Unofficial extension for Scrath (scratch.mit.edu). It makes possibile actual (browser native) fullscreen mode.
+
+## Features
+
+- Browser's native fullscreen
+- Disappear rounded borders
+
+## How to use
+
+Install this extension from Chrome Store. Then open Scratch project and click fullscreen button above your content.
+
+[![Image from Gyazo](https://i.gyazo.com/6bad48b978335f3ec71a6b6b829bc218.png)](https://gyazo.com/6bad48b978335f3ec71a6b6b829bc218)
---
+++ content.js
@@ -0,0 +1,36 @@
+(function bindAction() {
+  var length = document.images.length;
+  for (var i = 0; i < length; i++) {
+    var img = document.images.item(i);
+    if (img && img.className.indexOf('stage-button-icon') > -1) {
+      // bind
+      img.addEventListener('click', function (event) {
+        event.stopPropagation();
+        var canvas = document.getElementsByTagName('canvas').item(0);
+        if (canvas) {
+          try {
+            canvas.requestFullscreen();
+          } catch (error) {
+            console.warn(error);
+            return; //exit
+          }
+        }
+      });
+    }
+  }
+  setTimeout(bindAction, 500);
+})();
+
+(function disableBorderRadius() {
+  var canvas = document.getElementsByTagName('canvas').item(0);
+  if (canvas) {
+    try {
+      var e = canvas.parentElement.parentElement;
+      e.style.borderRadius = '0px';
+    } catch (error) {
+      console.warn(error);
+      return; //exit
+    }
+  }
+  setTimeout(disableBorderRadius, 500);
+})();
