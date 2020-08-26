1224fed9ebb950b35fa9176d0d98956b336cca6b
teramotodaiki <i101323@gmail.com>
2020-08-26 22:33:19 +0900

    bind to button instead of img

--- content.js
+++ content.js
@@ -1,29 +1,54 @@
 (function bindAction() {
-  var length = document.images.length;
-  for (var i = 0; i < length; i++) {
-    var img = document.images.item(i);
-    if (img && img.className.indexOf('stage-button-icon') > -1) {
-      // bind
-      img.addEventListener('click', function (event) {
-        event.stopPropagation();
-        var canvas = document.getElementsByTagName('canvas').item(0);
-        if (canvas) {
-          try {
-            canvas.requestFullscreen();
-          } catch (error) {
-            console.warn(error);
-            return; //exit
-          }
   // Support Chrome <69
   Element.prototype.requestFullscreen =
     Element.prototype.requestFullscreen ||
     Element.prototype.webkitRequestFullscreen;
 
+  var button = findElementByClassNameStartsWith(
+    document.body,
+    'stage-header_stage-button'
+  );
+  var canvas = document.getElementsByTagName('canvas').item(0);
+
+  if (!button || !canvas || !canvas.parentElement) {
+    return setTimeout(bindAction, 500); // retry
+  }
+
+  // bind
+  button.addEventListener(
+    'click',
+    function (event) {
+      event.stopPropagation();
+      try {
+        canvas.requestFullscreen();
+      } catch (error) {
+        console.warn(error);
+      }
+    },
+    { capture: true }
+  );
+
+  /**
+   * Find element like div.stage_green-flag-overlay-wrapper_2hUi_
+   * @param parentElement {Element}
+   * @param classNamePrefix {String}
+   * @returns {HTMLElement}
+   */
+  function findElementByClassNameStartsWith(parentElement, classNamePrefix) {
+    for (var i = 0; i < parentElement.children.length; i++) {
+      var element = parentElement.children[i];
+      for (var j = 0; j < element.classList.length; j++) {
+        var className = element.classList[j];
+        if (className.indexOf(classNamePrefix) === 0) {
+          return element;
         }
-      });
+      }
+      var result = findElementByClassNameStartsWith(element, classNamePrefix);
+      if (result !== undefined) {
+        return result;
+      }
     }
   }
-  setTimeout(bindAction, 500);
 })();
 
 (function disableBorderRadius() {
